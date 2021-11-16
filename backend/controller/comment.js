7bc0589f4b5c9d747e76915d6ef342d99b1c6542
Nicolas Poltorak <nicolaspoltorak@outlook.fr>
2021-11-16 14:11:46 +0100

    reset again

--- backend/controller/comment.js
+++
@@ -1,8 +0,0 @@
-const db = require("../models");
-const Comment = db.comment;
-
-exports.createComment = (req, res, next) => {};
-
-exports.findAllComments = (req, res, next) => {};
-
-exports.deleteComment = (req, res, next) => {};
--- backend/routes/like.routes.js
+++
@@ -1,9 +0,0 @@
-module.exports = (app) => {
-  const likes = require("../controller/like");
-
-  const router = require("express").Router();
-
-  router.patch("/", likes.createLike);
-
-  app.use("api/likes", router);
-};
